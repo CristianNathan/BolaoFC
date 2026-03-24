77cd39c4925cbd223ed0ea12616b1618479f183e
Cristian Nathan <cristometro43@gmail.com>
2026-03-23 21:52:57 -0300

    Implementando o sistema de autenticação e segurança

--- src/main/java/com/bolaofc/bolaofc/controller/AuthController.java
+++ src/main/java/com/bolaofc/bolaofc/controller/AuthController.java
@@ -1,8 +1,13 @@
 package com.bolaofc.bolaofc.controller;
 
+import com.bolaofc.bolaofc.infraSecurity.TokenService;
 import com.bolaofc.bolaofc.user.User;
 import com.bolaofc.bolaofc.user.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
     private final UserService userService;
-
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    TokenService tokenService;
     public AuthController(UserService userService){
         this.userService = userService;
     }
@@ -28,6 +36,15 @@ public class AuthController {
         this.userService.salvar(newUser);
         return ResponseEntity.ok().build();
     }
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid User data){
+        var usernameSenha = new UsernamePasswordAuthenticationToken(data.getEmail(), data.getSenha());
+        var auth = this.authenticationManager.authenticate(usernameSenha);
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+        return ResponseEntity.ok(token);
+    }
+
+
 
 }
 
--- src/main/java/com/bolaofc/bolaofc/user/User.java
+++ src/main/java/com/bolaofc/bolaofc/user/User.java
@@ -1,13 +1,19 @@
 package com.bolaofc.bolaofc.user;
 
 import jakarta.persistence.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Table(name="usuarios")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -83,5 +89,40 @@ public class User {
 
     public void setCriadoEm(LocalDateTime criadoEm) {
         this.criadoEm = criadoEm;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 }
