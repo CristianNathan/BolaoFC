cdeeb1b15daa176de220c45796b7a7c84ea854c5
Cristian Nathan <cristometro43@gmail.com>
2026-06-07 01:27:21 -0300

    Forçando o CORS

--- src/main/java/com/bolaofc/bolaofc/config/CorsConfig.java
+++ src/main/java/com/bolaofc/bolaofc/config/CorsConfig.java
@@ -3,7 +3,7 @@ package com.bolaofc.bolaofc.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//Forçando
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
